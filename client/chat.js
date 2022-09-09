e58e5fd759d11c8b1cf5a11432c0961639eb9076
Alan Shirk <ashirk94@gmail.com>
2022-09-09 16:11:55 -0700

    Livechat functional between websocket connections

--- client/chat.js
+++ client/chat.js
@@ -1,18 +1,38 @@
 import {io} from 'socket.io-client'
 
-const test = document.getElementById('test')
+//html elements
+const messageContainer = document.getElementById('message-container')
+const messageInput = document.getElementById('message-input')
+const form = document.getElementById('form')
 
+//creates div and appends with message
 function displayMessage(message) {
-    test.innerHTML = message
+    const div = document.createElement('div')
+    div.textContent = message
+    messageContainer.append(div)
 }
 
+//local socket
 const socket = io('http://localhost:3000')
 
 socket.on('connect', () => {
-    displayMessage(`You connected with id: ${socket.id}`)
-    console.log(socket.id)
+    console.log(`You connected with id: ${socket.id}`)
 })
 
+//recieving messages
+socket.on('receive-message', message => displayMessage(message))
+
+//send a message
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    //const room = roomInput.value
+
+    if (message === '') return
+    displayMessage(message)
+    socket.emit('send-message', message)
+    messageInput.value = ''
+})
 
 
 export default null
--- server/server.js
+++ server/server.js
@@ -5,5 +5,7 @@ const io = require('socket.io')(3000, {
 })
 
 io.on('connection', socket => {
-    //console.log(socket.id)
+    socket.on('send-message', message => {
+        socket.broadcast.emit('receive-message', message)
+    })
 })
