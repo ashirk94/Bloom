0627607aff7b0a04472177af19ada097da9cbf29
Alan Shirk <ashirk94@gmail.com>
2022-09-24 16:45:13 -0700

    User data update capabilities
    Profile picture uploads and displays

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,20 @@ const router = require('express').Router()
 const isAuth = require('../utilities/authMiddleware').isAuth
 const isAdmin = require('../utilities/authMiddleware').isAdmin
 const path = require('path')
+const connection = require('../config/database')
+const User = connection.models.User
+
 const multer = require('multer')
 const storage = multer.diskStorage({
     destination: (req, file, next) => {
-        next(null, '../images')
+        next(null, path.join(__dirname, '../public/images'))
     },
     filename: (req, file, next) => {
-        console.log(file)
         next(null, Date.now() + path.extname(file.originalname))
     }
 })
 const upload = multer({storage: storage})
+
 //get routes
 
 router.get('/', (req, res) => {
@@ -37,7 +40,24 @@ router.get('/admin', isAdmin, (req, res) => {
 
 //post
 
-router.post('/profile', isAuth, upload.single('image'), (req, res) => {
-    res.send('image uploaded')
+router.post('/profile', isAuth, upload.single('image'), async (req, res) => {
+    try {
+        let user = req.user
+        if (req.body.firstName != '') {
+            user.firstName = req.body.firstName
+        }
+        if (req.body.lastName != '') {
+            user.lastName = req.body.lastName
+        }
+        
+        user.profilePic = req.file.filename
+        
+        let updatedUser = await User.findOneAndUpdate(user.id, user)
+        await updatedUser.save()
+    } catch(err) {
+        console.error(err)
+        res.redirect('/profile')
+    }
+    res.redirect('/')
 })
 module.exports = router
