cf83ccdfaaf4d085094376965598b066d0ddd9e7
Alan Shirk <ashirk94@gmail.com>
2022-09-26 13:49:26 -0700

    Interests toggle on front end

--- public/interests.js
+++ public/interests.js
@@ -4,20 +4,36 @@ const gameBtn = document.getElementById('game')
 const artBtn = document.getElementById('art')
 const baseballBtn = document.getElementById('baseball')
 
-let interests = document.querySelectorAll('.interest')
+let interestData = document.querySelectorAll('.interest')
+let interests = []
+for (let interest of interestData) {
+    interests.push(interest)
+}
+
+//need better values based on different lifestyles
+let valueData = document.querySelectorAll('.value')
+let values = []
+for (let value of valueData) {
+    values.push(value)
+}
 
 let swimBool = false
 
-if (interests.find('swim')) {
+if (interests.find(interest => interest === 'Swimmming')) {
     swimBool = true
 }
 
 swimBtn.addEventListener('click', () => {
     if (swimBool) {
-        swimBool = false
+        let index = interests.findIndex(interest => interest === 'Swimmming')
+        interests.splice(index, 1)
         swimBtn.classList.remove('active')
-    } else if (interests.length > 4) {
-        swimBool = true
+        swimBool = false
+        
+    } else if (interests.length < 5) {      
         swimBtn.classList.add('active')
+        interests.push('Swimming')
+        swimBool = true
     }
+    console.log(interests)
 })
