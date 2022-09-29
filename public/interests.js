f68245ad939bafbb6ab74dc0dea9848f067d6def
Alan Shirk <ashirk94@gmail.com>
2022-09-29 13:50:49 -0700

    Multiple interest buttons adding to array

--- public/interests.js
+++ public/interests.js
@@ -19,7 +19,9 @@ let values = []
 for (let value of valueData) {
     if (value.value != '') values.push(value.value)  
 }
+//button toggle logic
 
+//swimming
 let swimBool = false
 
 if (interests.includes('Swimming')) {
@@ -39,9 +41,92 @@ swimBtn.addEventListener('click', () => {
         interests.push('Swimming')
         swimBool = true
     }
-    console.log(interests)
 })
+//baseball
+let baseballBool = false
 
+if (interests.includes('Baseball')) {
+    baseballBool = true
+    baseballBtn.classList.add('active')
+}
+
+baseballBtn.addEventListener('click', () => {
+    if (swimBool) {
+        let index = interests.findIndex(interest => interest === 'Baseball')
+        interests.splice(index, 1)
+        baseballBtn.classList.remove('active')
+        baseballBool = false
+        
+    } else if (interests.length < 5) {      
+        baseballBtn.classList.add('active')
+        interests.push('Baseball')
+        baseballBool = true
+    }
+})
+//gaming
+let gameBool = false
+
+if (interests.includes('Gaming')) {
+    gameBool = true
+    gameBtn.classList.add('active')
+}
+
+gameBtn.addEventListener('click', () => {
+    if (gameBool) {
+        let index = interests.findIndex(interest => interest === 'Gaming')
+        interests.splice(index, 1)
+        gameBtn.classList.remove('active')
+        gameBool = false
+        
+    } else if (interests.length < 5) {      
+        gameBtn.classList.add('active')
+        interests.push('Gaming')
+        gameBool = true
+    }
+})
+//coding
+let codeBool = false
+
+if (interests.includes('Coding')) {
+    codeBool = true
+    codeBtn.classList.add('active')
+}
+
+codeBtn.addEventListener('click', () => {
+    if (codeBool) {
+        let index = interests.findIndex(interest => interest === 'Coding')
+        interests.splice(index, 1)
+        codeBtn.classList.remove('active')
+        codeBool = false
+        
+    } else if (interests.length < 5) {      
+        codeBtn.classList.add('active')
+        interests.push('Coding')
+        codeBool = true
+    }
+})
+//art
+let artBool = false
+
+if (interests.includes('Art')) {
+    artBool = true
+    artBtn.classList.add('active')
+}
+
+artBtn.addEventListener('click', () => {
+    if (artBool) {
+        let index = interests.findIndex(interest => interest === 'Art')
+        interests.splice(index, 1)
+        artBtn.classList.remove('active')
+        artBool = false
+        
+    } else if (interests.length < 5) {      
+        artBtn.classList.add('active')
+        interests.push('Art')
+        cartBool = true
+    }
+})
+//submit data
 form.addEventListener('submit', event => {
     event.preventDefault()
     valueSubmit.value = JSON.stringify(values)
