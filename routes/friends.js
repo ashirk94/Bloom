2b7a682e90fd991a8cb93eb4ab58a80003f8ea10
Alan Shirk <ashirk94@gmail.com>
2022-09-30 19:44:25 -0700

    Fixed friend id route

--- routes/friends.js
+++ routes/friends.js
@@ -9,15 +9,15 @@ router.get('/chat', isAuth, (req, res) => {
 })
 
 router.get('/meet', isAuth, async (req, res) => {
-	const id = req.user.id
+	const id = req.user._id
 	const users = await User.find({ _id: { $ne: id } }).limit(5)
 	res.render('meet', { user: req.user, friends: users })
 })
 
-router.get('/friend/:user_id', isAuth, async (req, res) => {
+router.get('/:user_id', isAuth, async (req, res) => {
     const id = req.params.user_id.toString()  
-    const user = await User.findById(id)
-    res.render('friend', {friend: user})
+    const friend = await User.findById(id)
+    res.render('friend', {friend: friend, user: req.user})
 })
 
 module.exports = router
