b60dcbccb77edeeb59bbeb2b02590ff9740c874a
Alan Shirk <ashirk94@gmail.com>
2022-10-11 20:43:36 -0700

    Fixed socket user object and time format

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js"
+import { io } from 'https://cdn.socket.io/4.4.1/socket.io.esm.min.js'
 
 //html elements
 const messageContainer = document.getElementById('message-container')
@@ -11,78 +11,49 @@ const roomName = document.getElementById('room-name')
 
 //creates div and appends with message
 function displayMessage(message) {
-    //console.log(message)
-    const div = document.createElement('div')
-    div.textContent = message.time + '\n' + message.username + ': ' + message.text
-    div.classList.add('message')
-    messageContainer.append(div)
+	const div = document.createElement('div')
+	div.textContent =
+		message.time + '\n' + message.username + ': ' + message.text
+	div.classList.add('message')
+	messageContainer.append(div)
 }
 
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
-    ignoreQueryPrefix: true,
-  });
-  
-  const socket = io('http://localhost:3000');
-  
-  // Join chatroom
-  socket.emit('join-room', { username, room });
-  
-  // Message from server
-  socket.on('receive-message', (user, message) => {
-    console.log(message);
-    console.log(user)
-    displayMessage(user.message);
-  
-    // Scroll down
-    //chatMessages.scrollTop = chatMessages.scrollHeight;
-  });
-  
-  // Message submit
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-  
-    // Get message text
-    let msg = messageInput.value;
-  
-    msg = msg.trim();
-  
-    if (!msg) {
-      return false;
-    }
-    console.log(msg);
-    // Emit message to server
-    socket.emit('send-message', msg);
-    // Clear input
-    messageInput.value = '';
-    messageInput.focus();
-  });
+	ignoreQueryPrefix: true
+})
 
-//recieving messages
-// socket.on('receive-message', message =>  {
-//     console.log(message)
-//     if (message === '') return
-//     displayMessage(message)
-// })
-//send a message
-// form.addEventListener('submit', e => {
-//     e.preventDefault()
-//     const messageText = messageInput.value
-//     const room = roomInput.value
+const socket = io('http://localhost:3000')
 
+// Join chatroom
+socket.emit('join-room', { username, room })
 
-//     socket.emit('send-message', messageText, room)
-//     displayMessage(messageText)
-//     messageInput.value = ''
-// })
+// Message from server
+socket.on('receive-message', (user, message) => {
+	displayMessage(user.message)
 
-// joinRoomButton.addEventListener('click', () => {
-//     const room = roomInput.value
-//     if (room === '') return
-//     socket.emit('join-room', user, room, message => {
-//         displayMessage(message)
-//     })
-// })
+	// Scroll down
+	//chatMessages.scrollTop = chatMessages.scrollHeight;
+})
 
+// Message submit
+form.addEventListener('submit', (e) => {
+	e.preventDefault()
+
+	// Get message text
+	let msg = messageInput.value
+
+	msg = msg.trim()
+
+	if (!msg) {
+		return false
+	}
+
+	// Emit message to server
+	socket.emit('send-message', msg)
+	// Clear input
+	messageInput.value = ''
+	messageInput.focus()
+})
 
 export default null
