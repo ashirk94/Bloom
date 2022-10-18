4c9ba1b99f6eb2a5bd67c5e6e3e7e96097ac53d4
Alan Shirk <ashirk94@gmail.com>
2022-10-18 14:55:13 -0700

    Private chats display correct messages

--- routes/messages.js
+++ routes/messages.js
@@ -3,16 +3,16 @@ const isAuth = require('../utilities/authMiddleware').isAuth
 const connection = require('../config/database')
 const User = connection.models.User
 
-//sends a message from a user to another
-router.post('/message/:id', isAuth, async (req, res) => {
-	const user = await User.findById(req.params.id)
+// //sends a message from a user to another
+// router.post('/message/:id', isAuth, async (req, res) => {
+// 	const user = await User.findById(req.params.id)
 
-	//stops user from liking the same one multiple times
-	if (!user.likes.some((x) => x.username === req.user.username)) {
-		user.likes.push(req.user)
-		await user.save()
-	}
-})
+// 	//stops user from liking the same one multiple times
+// 	if (!user.likes.some((x) => x.username === req.user.username)) {
+// 		user.likes.push(req.user)
+// 		await user.save()
+// 	}
+// })
 
 //gets messages
 router.get('/message/:id', isAuth, async (req, res) => {
