4d9c204a29b98392d071bc032f9acb052c0fdfbd
Alan Shirk <ashirk94@gmail.com>
2022-11-26 11:51:53 -0800

    Animated navbar

--- public/navigation.js
+++ public/navigation.js
@@ -2,15 +2,20 @@ const navbarToggle = document.getElementById('navbar-toggle')
 const navLinks = document.querySelectorAll('.nav-link')
 const navItems = document.querySelectorAll('.nav-item')
 const lowBar = document.getElementById('lower-navbar')
+const topBar = document.getElementById('upper-navbar')
+const navBar = document.getElementById('navbar')
 
 function toggleBar() {
-    for (const navLink of navLinks) {
-        navLink.classList.toggle('open') 
-    }
-    for (const navItem of navItems) {
-        navItem.classList.toggle('mobile-invis') 
-    }
+	navBar.classList.toggle('pop-in')
     lowBar.classList.toggle('mobile-invis')
-  }
+    topBar.classList.toggle('appear')
+    lowBar.classList.toggle('appear')
+    for (const navItem of navItems) {
+		navItem.classList.toggle('mobile-invis')
+	}
+	for (const navLink of navLinks) {
+		navLink.classList.toggle('open')
+	}
+}
 
-navbarToggle.addEventListener("click", toggleBar)
+navbarToggle.addEventListener('click', toggleBar)
